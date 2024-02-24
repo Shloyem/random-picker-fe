374c6c0c074cf311d4104173dd4b31ff9bc0c0d8
shloyem <39220943+Shloyem@users.noreply.github.com>
2024-02-24 12:46:42 +0200

    Add initial version of elements and link creation and display

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+// Run by command: npm start
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [options, setOptions] = useState(['Option  1', 'Option  2']);
+  const [link, setLink] = useState('');
+
+  const createRandomSelection = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/create', {
+        options,
+      });
+      console.log('Received created ID: %s', response.data.id);
+      setLink(`http://localhost:3001/result/${response.data.id}`);
+    } catch (error) {
+      console.error('Failed to create random selection', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <input
+            type="text"
+            placeholder="Option  1"
+            value={options[0]}
+            onChange={(e) => setOptions([e.target.value, options[1]])}
+          />
+          <input
+            type="text"
+            placeholder="Option  2"
+            value={options[1]}
+            onChange={(e) => setOptions([options[0], e.target.value])}
+          />
+          <button onClick={createRandomSelection}>
+            Create Random Selection
+          </button>
+          {link && <p>Share this link: {link}</p>}
+        </div>
       </header>
     </div>
   );
