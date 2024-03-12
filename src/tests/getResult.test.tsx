3e1d29bc9110b82c7fe129580f9b500c3de75c70
shloyem <39220943+Shloyem@users.noreply.github.com>
2024-03-12 14:04:40 +0200

    Move mock to separate file

--- src/tests/getResult.test.tsx
+++ src/tests/getResult.test.tsx
@@ -1,27 +1,11 @@
 import getResult from '../services/getResult';
 import axiosInstance from '../services/axiosInstance';
 import { Draw } from '../types/Draw';
+import { drawMock } from './mocks/drawMock';
 
 jest.mock('../services/axiosInstance', () => ({
   get: jest.fn(),
 }));
-
-// Export to file
-const drawMock: Draw = {
-  options: [
-    'Option 1',
-    'Option 2',
-    'Option 3',
-    'Option 4',
-    'Option 5',
-    'Option 6',
-    'Option 7',
-  ],
-  result: 'Option 6',
-  createdAt: new Date(1709752108401),
-  drawAt: new Date(1709752111035),
-  expiresAt: new Date(1709838508401),
-};
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -34,4 +18,5 @@ it('Should return a json object on successful call', async () => {
   const result = await getResult('mockID');
 
   expect(result).toEqual(drawMock);
+  expect(result).toBeTruthy();
 });
---
+++ src/tests/mocks/drawMock.ts
@@ -0,0 +1,19 @@
+import { Draw } from '../../types/Draw';
+
+const drawMock: Draw = {
+  options: [
+    'Option 1',
+    'Option 2',
+    'Option 3',
+    'Option 4',
+    'Option 5',
+    'Option 6',
+    'Option 7',
+  ],
+  result: 'Option 6',
+  createdAt: new Date(1709752108401),
+  drawAt: new Date(1709752111035),
+  expiresAt: new Date(1709838508401),
+};
+
+export { drawMock };
