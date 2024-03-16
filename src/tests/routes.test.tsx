bbc34d00bc6b46096260877ac6c8b0cb17ae49c2
shloyem <39220943+Shloyem@users.noreply.github.com>
2024-03-16 16:12:03 +0200

    Add another way to React component test

--- src/tests/routes.test.tsx
+++ src/tests/routes.test.tsx
@@ -1,3 +1,5 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import Layout from '../routes/Layout';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -12,4 +14,15 @@ it('should check layout has home link', () => {
   );
 
   expect(div.innerHTML.indexOf('<a href="/">Home</a>')).toBeGreaterThan(0);
+});
+
+it('should check layout has home link - tested different way for learning', () => {
+  render(
+    <MemoryRouter>
+      <Layout />
+    </MemoryRouter>,
+  );
+
+  const link = screen.getByText('Home');
+  expect(link).toBeInTheDocument();
 });
